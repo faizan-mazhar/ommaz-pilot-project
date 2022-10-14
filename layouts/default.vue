68faee782e9fa26382c31c47dd13c3387564dca0
Faizan Mazhar <faizanmazher@hotmail.com>
2022-10-14 19:59:25 +0500

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <HomePage/>
+  </div>
+</template>
+<script lang="ts">
+import {
+defineComponent
+} from '@nuxtjs/composition-api';
+
+import HomePage from '../pages/Home.vue';
+
+export default defineComponent({
+  name: 'DefaultLayout',
+
+  components: {
+    HomePage
+},
+
+  // setup() {
+  //   const route = useRoute();
+
+  //   return {
+  //     route,
+  //   };
+  // },
+  head: {
+    link: [{ rel: 'stylesheet', href: '/_nuxt/fonts.css' }],
+  },
+});
+</script>
+
+<style lang="scss">
+@import "~@storefront-ui/vue/styles";
+
+#layout {
+  box-sizing: border-box;
+  @include for-desktop {
+    max-width: 1270px;
+    margin: auto;
+  }
+}
+
+.no-scroll {
+  overflow: hidden;
+  height: 100vh;
+}
+
+// Reset CSS
+html {
+  width: auto;
+  @include for-mobile {
+    overflow-x: hidden;
+  }
+}
+
+body {
+  overflow-x: hidden;
+  color: var(--c-text);
+  font-size: var(--font-size--base);
+  font-family: var(--font-family--primary);
+  margin: 0;
+  padding: 0;
+}
+
+a {
+  text-decoration: none;
+  color: var(--c-link);
+
+  &:hover {
+    color: var(--c-link-hover);
+  }
+}
+
+h1 {
+  font-family: var(--font-family--secondary);
+  font-size: var(--h1-font-size);
+  line-height: 1.6;
+  margin: 0;
+}
+
+h2 {
+  font-family: var(--font-family--secondary);
+  font-size: var(--h2-font-size);
+  line-height: 1.6;
+  margin: 0;
+}
+
+h3 {
+  font-family: var(--font-family--secondary);
+  font-size: var(--h3-font-size);
+  line-height: 1.6;
+  margin: 0;
+}
+
+h4 {
+  font-family: var(--font-family--secondary);
+  font-size: var(--h4-font-size);
+  line-height: 1.6;
+  margin: 0;
+}
+</style>
---
+++ pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts" type="module">
+import { defineComponent } from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  name: "HomePage",
+  components: {},
+
+  setup() {},
+});
+</script>
+
+<style lang="scss" scoped></style>
